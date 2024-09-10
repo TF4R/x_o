3d90142735eae01bd0d3c88446e0d9b2e9904ed3
Your Name <you@example.com>
2024-09-10 18:10:47 +0300

    test

--- test/src/App.js
+++ test/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { useState } from 'react';
+import './App.css';
+import App from './App';
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue('X');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button className="square" onClick={handleClick}>
+          {value}
+      </button>
   );
 }
 
-export default App;
+
+// ...
+export default function Board() {
+    return (
+        <>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+        </>
+    );
+}
